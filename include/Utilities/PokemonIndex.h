9c071b4aa7f76bc70bb8693b20e6bc09c038d1a3
Shahar Asher <shaharas30@gmail.com>
2023-06-21 19:22:16 +0300

    ./

--- include/Utilities/PokemonIndex.h
+++ include/Utilities/PokemonIndex.h
@@ -5,25 +5,24 @@
 #include <string>
 #include "utilities.h"
 
+
 std::vector<std::string> pokemonFiles { "1", "4", "6",
 										"7", "17", "25",
 										"56", "69", "88" };
 
-enum class PokemonIndex
-{
-	BULBASOR = 1,
-	CHARMANDER = 4,
-	CHARIZARD = 6,
-	SQUIRTLE = 7,
-	PIGEOT = 17,
-	PIKACHU = 25,
-	MONKEY = 56,
-	LEAF = 69,
-	MUD = 88,
-	////.......
-
-	END
-};
+enum class PokemonIndex { BULBASOR = 1,
+						  CHARMANDER = 4,
+						  CHARIZARD = 6,
+						  SQUIRTLE = 7,
+						  PIGEOT = 17,
+						  PIKACHU = 25,
+						  MONKEY = 56,
+						  LEAF = 69,
+						  MUD = 88,
+	
+						////.......
+	
+						  END };
 
 std::vector <PokemonIndex> FirstLevel { PokemonIndex::BULBASOR,
 										PokemonIndex::PIGEOT,
@@ -43,3 +42,4 @@ const std::unordered_map<enum PokemonIndex, std::string> pocimonNames = { { Poke
 	{ PokemonIndex::MONKEY, "MONKEY" },
 	{ PokemonIndex::LEAF, "LEAF" },
 	{ PokemonIndex::MUD, "MUD" } };
+
