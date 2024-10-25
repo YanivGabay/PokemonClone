24c4da3940fa1ce04cef68eb407d7933b6cca00e
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-10-25 15:13:15 +0300

    added some debuging printing for the state stack and the que

--- include/Stack.h
+++ include/Stack.h
@@ -5,6 +5,8 @@
 #include <queue>
 
 
+
+
 template <class T>
 class Stack
 {
@@ -16,23 +18,39 @@ public:
 	
 	void pushState(std::unique_ptr<T> state)
 	{
+		std::cout << "pushState" << "   pushing state: ";
+		printDebugState(typeid(state).name());
+	    std::cout << std::endl;
+
+		printInfoDebug();
 		m_states.push_back(std::move(state));
+		std::cout << "\nafter PushState" << std::endl;
+		printInfoDebug();
 	}
 	
 	void popStart()
 	{
+		std::cout << "popStart" << std::endl;
+		printInfoDebug();
 		if (!m_states.empty())
 		{
 			m_states.erase(m_states.begin());
 		}
+		std::cout << "\nafter popStart" << std::endl;
+		printInfoDebug();
 	}
 	
 	void popState()
 	{
+		std::cout << "popState" << std::endl;
+		printInfoDebug();;
+		
 		if (!m_states.empty())
 		{
 			m_states.pop_back();
 		}
+		std::cout << "\nafter popState" << std::endl;
+		printInfoDebug();
 	}
 
 	std::unique_ptr<T>& back()
@@ -110,14 +128,78 @@ public:
 
 		return m_states.size();
 	}
+	void printDebugState(const char* name)
+	{
+		std::cout << name << std::endl;
+	}
+
 	
 	void printTypeOf()
-	{
+	{ 
+
+
+		std::cout << "//////////////////////////"
+			<< " STATE STACK "
+			<< "//////////////////////////\n";
+		if(m_states.size())
 		for (std::size_t i = 0; i < m_states.size(); ++i)
 		{
 			std::cout << "Item at position " << i << ": ";
-			std::cout << typeid(*m_states[i]).name() << std::endl;
+			printDebugState(typeid(*m_states[i]).name());
+			
 		}
+		std::cout << "Total stack size: " << m_states.size() << "\n\n";
+
+		// Print queue
+		printQueueTypeOf();
+		
+	}
+	void printInfoDebug()
+	{
+		std::cout << "!!!! info !!!\n";
+			printTypeOf();
+			std::cout << "!!!! finished !!!\n";
+	}
+	
+	
+	void printQueueTypeOf()
+	{
+		std::cout << "//////////////////////////"
+			<< " STATE QUEUE "
+			<< "//////////////////////////\n";
+
+		if (m_stateQueue.empty())
+		{
+			std::cout << "stateQueue is empty" << std::endl;
+			return;
+		}
+
+		int position = 0;
+		std::size_t originalSize = m_stateQueue.size(); // Store the original size
+
+		for (std::size_t i = 0; i < originalSize; ++i)
+		{
+			// Access the front element
+			const T* element = m_stateQueue.front().get(); // Get raw pointer
+
+			if (element)
+			{
+				std::cout << "Item at position " << position << ": ";
+				std::cout << typeid(*element).name() << std::endl;
+			}
+			else
+			{
+				std::cout << "Item at position " << position << ": " << "nullptr" << std::endl;
+			}
+
+			// Move the front element to the back to maintain queue order
+			m_stateQueue.push(std::move(m_stateQueue.front()));
+			m_stateQueue.pop();
+
+			++position;
+		}
+
+		std::cout << "Total queue size: " << originalSize << "\n";
 	}
 	
 	bool checkQuitStatus()
